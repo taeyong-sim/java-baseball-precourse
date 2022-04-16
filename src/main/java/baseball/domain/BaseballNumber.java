2421e99898c6afcb2cf274afac823f99db394853
taeyong-sim <sim2ty@naver.com>
2022-04-17 00:02:56 +0900

    fix : 미사용 메서드 제거 및 for문 수정

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -19,10 +19,6 @@ public class BaseballNumber {
         }
     }
 
-    /*public BaseballNumber(int inputNumber) {
-        this.baseballNumberList = storeInputNumber(inputNumber);
-    }*/
-
     private boolean validate(String inputString) {
         String regex = "^[1-9]{3}$";
 
@@ -44,16 +40,10 @@ public class BaseballNumber {
         return baseballNumberList;
     }
 
-    /*private List<Integer> storeInputNumber(int inputNumber){
-        numberToList(inputNumber);
-
-        return baseballNumberList;
-    }*/
-
     private void numberToList(int inputNumber){
         String[] inputNumberArr = String.valueOf(inputNumber).split("");
-        for (int i = 0; i < inputNumberArr.length; i++) {
-            int digit = Integer.parseInt(inputNumberArr[i]);
+        for (String numberStr : inputNumberArr) {
+            int digit = Integer.parseInt(numberStr);
             isValid(digit);
             baseballNumberList.add(digit);
         }
@@ -84,9 +74,5 @@ public class BaseballNumber {
     public void addDigit(int digit){
         baseballNumberList.add(digit);
     }
-
-    /*public void setBaseballNumberList(List<Integer> baseballNumberList) {
-        this.baseballNumberList = baseballNumberList;
-    }*/
 
 }
