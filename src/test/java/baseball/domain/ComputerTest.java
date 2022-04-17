3baae6c87ac42b328329188e9364a24b0e823216
taeyong-sim <sim2ty@naver.com>
2022-04-17 22:04:11 +0900

    fix : ComputerTest 공통로직 추출

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,17 +11,22 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Computer Class TEST")
 class ComputerTest {
 
+    Computer computer;
+
+    @BeforeEach
+    void init(){
+        computer = new Computer();
+    }
+
     @DisplayName("Computer 초기화")
     @Test
     void computerInitTest(){
-        Computer computer = new Computer();
         assertNotNull(computer.generateRandomNumber());
     }
 
     @DisplayName("Computer 숫자 Size")
     @Test
     void computerRandomNumberSizeTest(){
-        Computer computer = new Computer();
         BaseballNumber computerNumber = computer.generateRandomNumber();
         assertEquals(computerNumber.getSize(), 3);
     }
