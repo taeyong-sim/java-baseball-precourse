bd050a80f922cf49e4d42d5ae4b66ec95d859b38
taeyong-sim <sim2ty@naver.com>
2022-04-17 22:06:56 +0900

    fix : PlayerTest 공통로직 추출

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -10,10 +12,16 @@ import java.util.Arrays;
 @DisplayName("Player class TEST")
 class PlayerTest {
 
+    Player player;
+
+    @BeforeEach
+    void init(){
+        player = new Player();
+    }
+
     @DisplayName("player 초기화")
     @Test
     void initPlayer(){
-        Player player = new Player();
         BaseballNumber baseballNumber = new BaseballNumber("123");
         player.setBaseballNumber(baseballNumber);
         assertEquals(new ArrayList<>(Arrays.asList(1,2,3)), player.getBaseballNumber().getBaseballNumberList());
